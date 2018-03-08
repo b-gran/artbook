e7d5bb8efea54f9a8a297b8141ac11582efcaa67
b-gran <bill@billgranfield.com>
2018-03-08 22:45:03 +0000

    Correctly support hosting on a web server

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom'
 import './App.css'
 import Static from './Static'
 import Squares from './RotatedSquares'
@@ -8,55 +8,61 @@ import BasicPerlin from './BasicPerlin'
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <div className="experiment-list">
-            <Link to='/basic-static'>
-              <div className="experiment-link">Static</div>
-            </Link>
+      <Router basename={process.env.PUBLIC_URL}>
+        <Switch>
+          <Route path='/a' render={() => (
+            <div className="App">
+              <div className="experiment-list">
+                <Link to='/a/basic-static'>
+                  <div className="experiment-link">Static</div>
+                </Link>
 
-            <Link to='/squares'>
-              <div className="experiment-link">Squares</div>
-            </Link>
+                <Link to='/a/squares'>
+                  <div className="experiment-link">Squares</div>
+                </Link>
 
-            <Link to='/straight-perlin'>
-              <div className="experiment-link">Perlin noise</div>
-            </Link>
-          </div>
+                <Link to='/a/straight-perlin'>
+                  <div className="experiment-link">Perlin noise</div>
+                </Link>
+              </div>
 
-          <Route exact path='/' render={() => <Redirect to='/straight-perlin'/>}/>
+              <Route exact path='/a' render={() => <Redirect to='/a/straight-perlin'/>}/>
 
-          <Route path='/basic-static' render={() => (
-            <div className='experiment'>
-              <h2 className='experiment-title'>Static</h2>
-              <p>
-                Generating some mostly regular static.
-              </p>
+              <Route path='/a/basic-static' render={() => (
+                <div className='experiment'>
+                  <h2 className='experiment-title'>Static</h2>
+                  <p>
+                    Generating some mostly regular static.
+                  </p>
 
-              <Static />
+                  <Static />
+                </div>
+              )}/>
+
+              <Route path='/a/squares' render={() => (
+                <div className='experiment'>
+                  <h2 className='experiment-title'>Rotated squares</h2>
+
+                  <Squares />
+                </div>
+              )}/>
+
+              <Route path='/a/straight-perlin' render={() => (
+                <div className='experiment'>
+                  <h2 className='experiment-title'>Perlin noise</h2>
+
+                  <p>
+                    Just some plain ol Perlin noise
+                  </p>
+
+                  <BasicPerlin />
+                </div>
+              )}/>
             </div>
-          )}/>
+          )} />
 
-          <Route path='/squares' render={() => (
-            <div className='experiment'>
-              <h2 className='experiment-title'>Rotated squares</h2>
-
-              <Squares />
-            </div>
-          )}/>
-
-          <Route path='/straight-perlin' render={() => (
-            <div className='experiment'>
-              <h2 className='experiment-title'>Perlin noise</h2>
-
-              <p>
-                Just some plain ol Perlin noise
-              </p>
-
-              <BasicPerlin />
-            </div>
-          )}/>
-        </div>
+          <Route path='*' render={() => <Redirect to='/a'/>} />
+        </Switch>
       </Router>
     )
   }
